dc0b1242cde60e21bebd39c217407b2baf6076cd
David Chung <joonki34@gmail.com>
2025-08-23 22:25:07 +0900

    Refactor movie data retrieval to use Megabox API instead of CGV webpage. Update movie list parsing and Telegram message formatting.

--- app/app.py
+++ app/app.py
@@ -1,29 +1,33 @@
 import json
 import os
 import requests
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
 
 
 def lambda_handler(event, context):
-    # Get information from CGV webpage
-    html = urlopen("http://www.cgv.co.kr/movies/?lt=1&ft=0")
-    bs_object = BeautifulSoup(html, "html.parser", from_encoding="utf-8")
+    # Get information from Megabox API
+    url = "https://www.megabox.co.kr/on/oh/oha/Movie/selectMovieList.do"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+        "Accept": "application/json"
+    }
+    response = requests.get(url, headers=headers)
+    data = response.json()
 
     result = ""
-
-    movie_list = bs_object.find_all("div", "box-contents")
+    movie_list = data.get("movieList", [])
     for index, movie_item in enumerate(movie_list):
-        title = movie_item.find("strong", "title").text
-        release_date = movie_item.find("span", "txt-info").find("strong").contents[0].get_text(strip=True)
-        result += (str(index + 1) + ". " + title + " " + release_date + "\n")
+        title = movie_item.get("movieNm", "")
+        release_date = movie_item.get("rfilmDe", "")
+        result += f"{index + 1}. {title} {release_date}\n"
 
     # Send to telegram
     bot_token = os.environ.get('TOKEN')
     chat_id = os.environ.get('CHAT_ID')
-    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + chat_id + \
-                '&parse_mode=HTML&text=' + result
-    response = requests.get(send_text)
+    send_text = (
+        f'https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}'
+        f'&parse_mode=HTML&text={result}'
+    )
+    requests.get(send_text)
 
     return {
         'statusCode': 200,
